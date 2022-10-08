b93126ae215d29ad6d0adba8d0697096ba962290
Silvio <silviu074@gmail.com>
2022-10-09 00:42:33 +0300

    changed the size for main components, added structure for swiper 1x5

---
+++ src/components/Swiper1x5.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import '../styles/MainFeed.css'
+
+export const Swiper1x5 = () => {
+  return (
+    <div className='swiper1x5Carousel '>
+
+      <div className='swiper1x5Element '>
+        <img className='swiper1x5Image' src='https://source.unsplash.com/random/300x300' 
+          alt='Random cover'></img>
+          <div>
+          <p>Playlist name</p>
+          <p>Playlist creator name</p>
+          </div>
+       </div>
+
+       <div className='swiper1x5Element '>
+        <img className='swiper1x5Image' src='https://source.unsplash.com/random/300x300' 
+          alt='Random cover'></img>
+          <div>
+          <p>Playlist name</p>
+          <p>Playlist creator name</p>
+          </div>
+       </div>
+
+       <div className='swiper1x5Element '>
+        <img className='swiper1x5Image' src='https://source.unsplash.com/random/300x300' 
+          alt='Random cover'></img>
+          <div>
+          <p>Playlist name</p>
+          <p>Playlist creator name</p>
+          </div>
+       </div>
+
+       <div className='swiper1x5Element '>
+        <img className='swiper1x5Image' src='https://source.unsplash.com/random/300x300' 
+          alt='Random cover'></img>
+          <div>
+          <p>Playlist name</p>
+          <p>Playlist creator name</p>
+          </div>
+       </div>
+
+       <div className='swiper1x5Element '>
+        <img className='swiper1x5Image' src='https://source.unsplash.com/random/300x300' 
+          alt='Random cover'></img>
+          <div>
+          <p>Playlist name</p>
+          <p>Playlist creator name</p>
+          </div>
+       </div>
+
+    </div>
+  )
+}
